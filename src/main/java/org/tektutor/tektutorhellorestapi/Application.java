8e3715de5a655d7c5b90239e619cf6a9884063c8
Jeganathan Swaminathan <mail2jegan@gmail.com>
2019-07-23 16:31:21 +0530

    Initial commit.

---
+++ src/main/java/org/tektutor/tektutorhellorestapi/Application.java
@@ -0,0 +1,13 @@
+package org.tektutor.tektutorhellorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
